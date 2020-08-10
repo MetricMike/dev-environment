c5d67279ce1d2d2597dbdca1d39121be69ce7cac
Michael Weigle <michael.weigle@gmail.com>
2020-08-10 18:23:26 -0400

    updated bootstrap to be less version dependent / more idempotent

--- bootstrap-ansible.sh
+++ bootstrap-ansible.sh
@@ -1,75 +1,68 @@
 #! /bin/bash
-
-# Install anyenv and only the bare minimum required for ansible 3
 # This script should be idempotent and safe to run multiple times
 
+# Enable universe repo
+RELEASE=$(lsb_release -sc)
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu ${RELEASE} universe multiverse"
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu ${RELEASE}-updates universe multiverse"
+
+# Install asdf and only the bare minimum required for ansible 3
 sudo apt -y update
 sudo apt -y upgrade
 sudo apt -y dist-upgrade
 sudo apt -y autoremove
 sudo apt -y autoclean
 
-if [ ! -d "~/.asdf" ]
+sudo apt -y install curl git
+
+if [[ -d "${HOME}/.asdf" ]];
 then
-    git clone https://github.com/asdf-vm/asdf.git ~/.asdf 2> /dev/null
+    asdf update
+else
+    git clone https://github.com/asdf-vm/asdf.git ${HOME}/.asdf
 fi
 
-cd ~/.asdf
-git fetch --all
-git checkout "$(git describe --abbrev=0 --tags)"
+# I don't know and I don't care to learn how to do multiline grep search/replace
+# so I'm just stealing from the /etc/profile & /etc/profile.d strategy
+mkdir -p ${HOME}/.startup.d
+mkdir -p ${HOME}/.shutdown.d
 
-# I don't know and I don't care to learn how to do multiline grep search/replace so
-# I'm just stealing from the /etc/profile & /etc/profile.d strategy
-mkdir -p ~/.startup.d
-mkdir -p ~/.shutdown.d
+# cat <<-'EOF' > ${HOME}/.startup.d/ssh.sh
+# #! /bin/bash
+#
+# if [[ ! -S ${HOME}/.ssh/ssh_auth_sock ]]; then
+#     eval `ssh-agent`
+#     ln -sf "$SSH_AUTH_SOCK" ${HOME}/.ssh/ssh_auth_sock
+# fi
+#
+# export SSH_AUTH_SOCK=${HOME}/.ssh/ssh_auth_sock
+# ssh-add -l > /dev/null || ssh-add ${HOME}/.ssh/mkey_big
+#
+# EOF
 
-if [ ! -e ~/.startup.d/ssh.sh ]
-then
-    cat <<-'EOF' > ~/.startup.d/ssh.sh
-#! /bin/bash
+# cat <<-'EOF' > ${HOME}/.shutdown.d/ssh.sh
+# #! /bin/bash
+#
+# if [[ -n "$SSH_AUTH_SOCK" ]] ; then
+#   eval `/usr/bin/ssh-agent -k`
+#   rm ${HOME}/.ssh/ssh_auth_sock
+# fi
+#
+# EOF
 
-if [ ! -S ~/.ssh/ssh_auth_sock ]; then
-    eval `ssh-agent`
-    ln -sf "$SSH_AUTH_SOCK" ~/.ssh/ssh_auth_sock
-fi
-
-export SSH_AUTH_SOCK=~/.ssh/ssh_auth_sock
-ssh-add -l > /dev/null || ssh-add ~/.ssh/mkey_big
-
-EOF
-fi
-
-if [ ! -e ~/.shutdown.d/ssh.sh ]
-then
-    cat <<-'EOF' > ~/.shutdown.d/ssh.sh
-#! /bin/bash
-
-if [ -n "$SSH_AUTH_SOCK" ] ; then
-  eval `/usr/bin/ssh-agent -k`
-  rm ~/.ssh/ssh_auth_sock
-fi
-
-EOF
-fi
-
-if [ ! -e ~/.startup.d/asdf.sh ]
-then
-    cat <<-'EOF' > ~/.startup.d/asdf.sh
+cat <<-'EOF' > ${HOME}/.startup.d/asdf.sh
 #! /bin/bash
 
 source ${HOME}/.asdf/asdf.sh
 source ${HOME}/.asdf/completions/asdf.bash
 
 EOF
-fi
 
-if [ ! -e ~/.startup.sh ]
-then
-    cat <<-'EOF' > ~/.startup.sh
+cat <<-'EOF' > ${HOME}/.startup.sh
 #! /bin/bash
 
-if [ -d ~/.startup.d ]; then
-    for i in ~/.startup.d/*.sh; do
+if [[ -d ${HOME}/.startup.d ]]; then
+    for i in ${HOME}/.startup.d/*.sh; do
         if [ -r $i ]; then
             source $i
         fi
@@ -78,18 +71,16 @@ if [ -d ~/.startup.d ]; then
 fi
 
 EOF
-fi
 
-STARTUP_INIT='source ~/.startup.sh'
-grep -qxF "${STARTUP_INIT}" ~/.bashrc || echo -e "${STARTUP_INIT}" >> ~/.bashrc
+STARTUP_INIT='source ${HOME}/.startup.sh'
+grep -qxF "${STARTUP_INIT}" ${HOME}/.bashrc || \
+echo -e "${STARTUP_INIT}" >> ${HOME}/.bashrc
 
-if [ ! -e ~/.shutdown.sh ]
-then
-    cat <<-'EOF' > ~/.shutdown.sh
+cat <<-'EOF' > ${HOME}/.shutdown.sh
 #! /bin/bash
 
-if [ -d ~/.shutdown.d ]; then
-    for i in ~/.shutdown.d/*.sh; do
+if [[ -d ${HOME}/.shutdown.d ]]; then
+    for i in ${HOME}/.shutdown.d/*.sh; do
         if [ -r $i ]; then
             source $i
         fi
@@ -98,27 +89,28 @@ if [ -d ~/.shutdown.d ]; then
 fi
 
 EOF
-fi
 
-SHUTDOWN_INIT='source ~/.shutdown.sh'
-grep -qxF "${SHUTDOWN_INIT}" ~/.bash_logout || echo -e "${SHUTDOWN_INIT}" >> ~/.bash_logout
-
-source ~/.bashrc
+SHUTDOWN_INIT='source ${HOME}/.shutdown.sh'
+grep -qxF "${SHUTDOWN_INIT}" ${HOME}/.bash_logout || \
+echo -e "${SHUTDOWN_INIT}" >> ${HOME}/.bash_logout
 
 # Build environment for python
 sudo apt -y install make build-essential libssl-dev zlib1g-dev libbz2-dev \
-libreadline-dev libsqlite3-dev wget curl llvm-4.0-dev llvm-4.0-runtime \
-libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev \
-python-openssl git
+libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev xz-utils tk-dev \
+libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev
+
+source ${HOME}/.bashrc
+source ${HOME}/.startup.d/asdf.sh
 
 asdf plugin-add python
-asdf install python 3.7.3
-asdf global python 3.7.3
+asdf install python latest:3
+asdf global python $(asdf latest python)
 
-pip install -U pip
-pip install -U ansible
+pip install -U pip ansible
 
 asdf reshim python
 
 # Verify
 ansible --version
+
+exec ${SHELL} -l
