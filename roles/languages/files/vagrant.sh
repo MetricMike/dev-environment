5713a6e234af426160a8ed20d5d3b68973fe58b7
Michael Weigle <michael.weigle@gmail.com>
2021-03-11 14:51:26 -0500

    remove vagrant

--- roles/languages/files/vagrant.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-export VAGRANT_WSL_ENABLE_WINDOWS_ACCESS="1"
-export VAGRANT_DEFAULT_PROVIDER="hyperv"
-
-# Needed until https://github.com/hashicorp/vagrant/issues/11085 is resolved
-alias powershell="pwsh"
