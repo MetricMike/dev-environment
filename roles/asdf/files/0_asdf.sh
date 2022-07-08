7e23f3eb09287810324892ba5d86b7962e89aa13
Michael Weigle <michael.weigle@gmail.com>
2022-07-08 16:05:35 -0400

    actually include env tasks; more asdf plugins; change prompt

--- roles/asdf/files/0_asdf.sh
+++ roles/asdf/files/0_asdf.sh
@@ -5,3 +5,5 @@ source ${HOME}/.asdf/completions/asdf.bash
 export JAVA_HOME=$(asdf where java)
 export JDK_HOME=${JAVA_HOME}
 source ${HOME}/.asdf/plugins/dotnet-core/set-dotnet-home.bash
+
+asdf direnv setup --shell bash --version latest
--- roles/languages/files/0_prompt.sh
+++ roles/languages/files/0_prompt.sh
@@ -1,3 +1,4 @@
 #! /bin/bash
 
-eval "$(oh-my-posh init bash)"
+export OMP_THEME="$(brew --prefix oh-my-posh)/themes/blue-owl.omp.json"
+eval "$(oh-my-posh init bash --config ${OMP_THEME})"
