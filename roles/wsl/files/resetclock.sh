adf275556570947ca4afcfaa1570a485f28a3126
Michael Weigle <michael.weigle@gmail.com>
2022-07-08 13:18:58 -0400

    add wsl role/tasks

--- roles/wsl/files/resetclock.sh
+++ roles/wsl/files/resetclock.sh
@@ -2,5 +2,6 @@
 
 # https://github.com/microsoft/WSL/issues/5324
 # Needed in at least 20277.1 to force a resync after suspend/hibernate
-
-wsl.exe -u root hwclock -s &> /dev/null
+# https://github.com/microsoft/WSL/issues/8204
+# was fixed but regression with win 11 beta push
+sudo hwclock -s &> /dev/null
