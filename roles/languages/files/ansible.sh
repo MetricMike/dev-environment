dc33f30c0f2b423bbdc098d4df3fe65aab36ea24
Michael Weigle <michael.weigle@gmail.com>
2023-05-04 14:14:58 -0400

    fix deprecation from ansible 4
    also needed to `unset ANSIBLE_CALLBACK_WHITELIST`

--- roles/languages/files/ansible.sh
+++ roles/languages/files/ansible.sh
@@ -2,7 +2,7 @@
 
 MITOGEN_LOCATION=$(pip show mitogen | grep Location | awk '{print $2}')
 
-export ANSIBLE_CALLBACK_WHITELIST="profile_roles, profile_tasks, timer"
+export ANSIBLE_CALLBACKS_ENABLED="profile_roles, profile_tasks, timer"
 export ANSIBLE_LOCALHOST_WARNING="False"
 export ANSIBLE_STRATEGY_PLUGINS="~/.ansible/plugins/strategy:/usr/share/ansible/plugins/strategy:${MITOGEN_LOCATION}/ansible_mitogen/plugins/strategy"
 export ANSIBLE_STRATEGY="mitogen_linear"
