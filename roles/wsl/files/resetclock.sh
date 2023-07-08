2992b0eb81b91de2d2196ae955c15a745457d2b0
Michael Weigle <michael.weigle@gmail.com>
2023-07-08 17:22:29 -0400

    feat(os): chrony, podman

--- roles/wsl/files/resetclock.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-# https://github.com/microsoft/WSL/issues/5324
-# Needed in at least 20277.1 to force a resync after suspend/hibernate
-# https://github.com/microsoft/WSL/issues/8204
-# was fixed but regression with win 11 beta push
-sudo hwclock -s &> /dev/null
