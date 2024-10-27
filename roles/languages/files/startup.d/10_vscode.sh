2f5bddf63c214c4e02bbd03ae91d47e71d79ab3a
Michael Weigle <michael.weigle@gmail.com>
2024-10-26 23:16:08 -0400

    oop bug slipped in the latest vscode

--- roles/languages/files/startup.d/10_vscode.sh
+++ roles/languages/files/startup.d/10_vscode.sh
@@ -3,7 +3,8 @@
 # shellcheck disable=SC1090
 
 if [[ "${TERM_PROGRAM}" == "vscode" ]]; then
-  . "$(code --locate-shell-integration-path bash)"
+  # TODO: broken in 1.94.0 will be fixed in November 2024 release (1.95.0?) https://github.com/microsoft/vscode/issues/230584
+  # . "$(code --locate-shell-integration-path bash)"
   export EDITOR=code
 else
   export EDITOR=vim
