4be46e66fd132778c5f21d742883b3ea5d75b697
Michael Weigle <michael.weigle@gmail.com>
2024-11-03 19:37:13 -0500

    cache facts, they don't change all that often

--- roles/languages/files/startup.d/ansible.sh
+++ roles/languages/files/startup.d/ansible.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 
-export ANSIBLE_CALLBACKS_ENABLED="profile_roles, profile_tasks, timer"
+export ANSIBLE_CALLBACKS_ENABLED="profile_roles, profile_tasks, timer, community.general.unixy"
+export ANSIBLE_CACHE_PLUGIN="jsonfile"
+export ANSIBLE_CACHE_PLUGIN_CONNECTION="${XDG_CACHE_HOME:-$HOME/.cache}/ansible-cache"
 export ANSIBLE_LOCALHOST_WARNING="False"
 export ANSIBLE_PIPELINING="True"
 export ANSIBLE_USE_PERSISTENT_CONNECTIONS="True"
