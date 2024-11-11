f096f8844eefdd27b9b68b619c4b91b675b05448
Michael Weigle <michael.weigle@gmail.com>
2024-11-10 22:56:57 -0500

    re-enable mitogen for real

--- roles/languages/files/startup.d/ansible.sh
+++ roles/languages/files/startup.d/ansible.sh
@@ -1,8 +1,12 @@
 #! /bin/bash
 
+MITOGEN_LOCATION=$(pip show mitogen | grep Location | awk '{print $2}')
+
 export ANSIBLE_CALLBACKS_ENABLED="profile_roles, profile_tasks, timer, community.general.unixy"
 export ANSIBLE_CACHE_PLUGIN="jsonfile"
 export ANSIBLE_CACHE_PLUGIN_CONNECTION="${XDG_CACHE_HOME:-$HOME/.cache}/ansible-cache"
+export ANSIBLE_STRATEGY_PLUGINS="${HOME}/.ansible/plugins/strategy:/usr/share/ansible/plugins/strategy:${MITOGEN_LOCATION}/ansible_mitogen/plugins/strategy"
+export ANSIBLE_STRATEGY="mitogen_linear"
 export ANSIBLE_LOCALHOST_WARNING="False"
 export ANSIBLE_PIPELINING="True"
 export ANSIBLE_USE_PERSISTENT_CONNECTIONS="True"
