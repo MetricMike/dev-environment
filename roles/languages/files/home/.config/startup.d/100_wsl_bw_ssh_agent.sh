d9b2d501473001aab5eeb2651d35fa716608a3a2
Michael Weigle <michael.weigle@gmail.com>
2025-12-02 23:46:10 -0500

    polish bw/ssh

--- roles/languages/files/home/.config/startup.d/100_wsl_bw_ssh_agent.sh
+++ roles/languages/files/home/.config/startup.d/100_wsl_bw_ssh_agent.sh
@@ -1,5 +1,6 @@
 #! /bin/bash
 
 if [[ -z "${SSH_CLIENT}" ]]; then
+  # shellcheck disable=SC1090
   . ~/scripts/agent-bridge.sh
 fi
--- roles/languages/files/home/scripts/agent-bridge.sh
+++ roles/languages/files/home/scripts/agent-bridge.sh
@@ -1,16 +1,13 @@
 #! /bin/bash
 # https://www.rebelpeon.com/bitwarden-ssh-agent-on-wsl2/
-
-USERNAME=$(/mnt/c/WINDOWS/system32/cmd.exe /c echo %USERNAME% 2>/dev/null | tr -d "\r\n")
+# https://blog.jkwmoore.dev/bitwarden-desktop-client-as-ssh-agent-with-wsl.html
 
 # Ensure ~/.ssh directory exists with correct permissions
 if [ ! -d "$HOME/.ssh" ]; then
-    mkdir -m 700 "$HOME/.ssh"
+  mkdir -m 700 "$HOME/.ssh"
 fi
 
 export SSH_AUTH_SOCK=$HOME/.ssh/agent.sock
-ss -a | grep -q $SSH_AUTH_SOCK
-if [ $? -ne 0 ]; then
-    rm -f $SSH_AUTH_SOCK
-    ( setsid socat UNIX-LISTEN:$SSH_AUTH_SOCK,fork EXEC:"/mnt/c/Users/$USERNAME/AppData/Local/Microsoft/WinGet/Packages/albertony.npiperelay_Microsoft.Winget.Source_8wekyb3d8bbwe/npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork & ) >/dev/null 2>&1
-fi
+mkdir --parents "$(dirname "$SSH_AUTH_SOCK")"
+[ -S "$SSH_AUTH_SOCK" ] && rm --force "$SSH_AUTH_SOCK"
+( setsid socat UNIX-LISTEN:"$SSH_AUTH_SOCK",fork EXEC:"npiperelay.exe -ei -s //./pipe/openssh-ssh-agent",nofork 2>>/tmp/agent-bridge.log & ) >/dev/null 2>&1
